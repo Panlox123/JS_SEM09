54a27d2d3174acc1ae3666f56ed25c25b38c3b59
Panlox123 <167274732+Panlox123@users.noreply.github.com>
2024-10-12 18:40:28 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'b';
+
+  numero1 : number = 0;
+  numero2 : number = 0;
+  resultado : number = 0;
+
+  //Método para realizar la operación según la opción seleccionada
+  operar(operacion : string) {
+    switch (operacion) {
+      case 'suma':
+        this.resultado = this.numero1 + this.numero2;
+        break;
+      case 'resta':
+        this.resultado = this.numero1 - this.numero2;
+        break;
+      case 'multiplicacion':
+        this.resultado = this.numero1 * this.numero2;
+        break;
+      case 'division':
+        if (this.numero2 !== 0) {
+          this.resultado = this.numero1 / this.numero2;
+        } else {
+          this.resultado = NaN; //Manejar división por cero
+        }
+        break;
+      case 'potencia':
+        this.resultado = Math.pow(this.numero1, this.numero2);
+        break;      
+      default:
+        this.resultado = 0;
+        break;
+    }
+  }
 }
